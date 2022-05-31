b34d4fe2e48a338cfcd0513ece895427e69de0b0
Malika Nurfikria <malikanurfikria9@gmail.com>
2022-05-31 21:25:55 +0700

    menambahkan folder praktikum9&10

--- blog/application/controllers/MataKuliah.php
+++ blog/application/controllers/MataKuliah.php
@@ -1,5 +1,5 @@
 <?php
-class MataKuliah extends CI_Controller{
+class Matakuliah extends CI_Controller{
     // membuat method index
     public function index(){
          // akses model matakuliah
@@ -14,11 +14,60 @@ class MataKuliah extends CI_Controller{
      public function detail($id){
          // akses model matakuliah
          $this->load->model('matakuliah_model');
-         $siswa = $this->mahasiswa_model->getById($id);
-         $data['siswa'] = $siswa;
+         $matakuliah = $this->matakuliah_model->getById($id);
+         $data['matakuliah'] = $matakuliah;
          $this->load->view('layouts/header');
-         $this->load->view('mahasiswa/detail', $data);
+         $this->load->view('matakuliah/detail', $data);
          $this->load->view('layouts/footer');
-     }
+     } 
+     public function form() {
+        
+        // kirim ke view
+        $this->load->view('layouts/header');
+        $this->load->view('matakuliah/form');
+        $this->load->view('layouts/footer');
+}
+     public function save(){
+        // akses ke model matakkuliah
+        $this->load->model('matakuliah_model', 'matakuliah'); // 1
+        $_nama = $this->input->post('nama');
+        $_sks = $this->input->post('sks');
+        $_kode = $this->input->post('kode');
+
+        $data_matakuliah['nama'] = $_nama; 
+        $data_matakuliah['sks'] = $_sks; 
+        $data_matakuliah['kode'] = $_kode; 
+
+        if((!empty($_idedit))){ // update
+            $data_matakuliah['id'] = $_idedit;
+            $this->matakuliah->update($data_matakuliah);
+        }else{
+            // data baru 
+            $this->matakuliah->simpan($data_matakuliah);
+        }
+        redirect('matakuliah','refresh');
+    }
+    public function edit($id){
+        // akses model matakuliah
+        $this->load->model('matakuliah_model','matakuliah');
+        $obj_matakuliah =  $this->matakuliah->getById($id);
+        $data['obj_matakuliah'] = $obj_matakuliah;
+        $this->load->view('layouts/header');
+        $this->load->view('matakuliah/edit', $data);
+        $this->load->view('layouts/footer');
+    }
+    public function delete($id){
+        $this->load->model('matakuliah_model','matakuliah');
+        // Mengecek data matakuliah berdasarkan id
+        $data_matakuliah['id'] = $id;
+        $this->matakuliah->delete($data_matakuliah);
+        redirect('matakuliah','refresh');
+    }
+     public function __construct(){
+        parent::__construct();
+        if(!$this->session->userdata('logged_in')){
+            redirect('/login');
+        }
+    }
 }
 ?>
--- blog/application/views/dosen/index.php
+++ blog/application/views/dosen/index.php
@@ -5,10 +5,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DOSEN</title>
+    <script>
+function hapusDosen(pesan){
+    if (confirm(pesan)){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+</script>
+<?php
+$username = $this->session->userdata('username');
+if($username){
+    echo "<h2>Selamat Datang $username</h2>";
+}
+?>
 </head>
 <body>
     <div class="col-md-12">
-    <h3>Dosen</h3>
+    <h3>Daftar Dosen</h3>
     <table border="1" class="table">
         <thead>
             <tr>
@@ -30,8 +46,15 @@
                 <td><?php echo $dsn -> nidn ?></td>
                 <td><?php echo $dsn -> pendidikan ?></td>
                 <td>
-                    <a href="<?php echo "detail/$dsn->id"?>">Detail</a>
-                </td>
+    <a href="<?php echo base_url("index.php/dosen/detail/$dsn->id") ?>" 
+    class="btn btn-info btn-lg active" role="button" aria-pressed="true">Detail</a>
+    &nbsp;
+    <a href= <?php echo base_url("index.php/dosen/edit/$dsn->id") ?> 
+    class="btn btn-success btn-lg active" role="button" aria-pressed="true">Edit</a>
+    &nbsp;
+    <a href= <?php echo base_url("index.php/dosen/delete/$dsn->id") ?> class="btn btn-danger btn-lg active" role="button" aria-pressed="true" 
+    onclick=" return hapusDosen('Anda Yakin ingin menghapus dosen yang bernama <?php echo $dsn->nama ?>?')">Hapus</a>
+</td>
             </tr>
             <?php
             $nomor++;
@@ -39,5 +62,7 @@
             ?>
         </tbody>
     </table>
+    <a href="<?=base_url()?>index.php/dosen/form" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Tambah</a>
+</div>
 </body>
 </html>
